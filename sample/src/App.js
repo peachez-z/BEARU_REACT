6c3961252bfb192ef98763e5fd4aa426661de515
peachez-z <rkfld0911@gmail.com>
2023-12-07 22:59:00 +0900

    7일차 컴포넌트

---
+++ sample/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// class App extends React.Component {
+//   render() {
+//     return <h1>Hello</h1>;
+//   }
+// }
+// export default App;
+
+function App() {
+  return <h1>Hello</h1>;
+}
+
+export default App;
